8d0dac419072bef340f0056b661edd4bf479b2db
Leonardo <leosilvatanabi@gmail.com>
2023-12-26 17:26:05 -0300

    Atividade 5 - Cria CRUD de Usuários - Adicionado Swagger

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,72 @@
+﻿using Exo.WebApi.Models;
+using Exo.WebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exo.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuarioController(UsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        //get /api/usuarios
+        [HttpGet]
+        public IActionResult Listar()
+
+        {
+            return Ok(_usuarioRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+
+        {
+            _usuarioRepository.Cadastrar(usuario);
+            return StatusCode(201);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+
+        {
+             Usuario usuario = _usuarioRepository.BuscaPorId(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return Ok(usuario);
+        }
+
+
+        [HttpPut]
+        public IActionResult Atualizar(int id, Usuario usuario)
+
+        {
+            _usuarioRepository.Atualizar(id, usuario);
+            return StatusCode(204);
+        }
+
+        [HttpDelete]
+        public IActionResult Deletar (int id) 
+        {
+            try
+            {
+                _usuarioRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);  
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
 using Exo.WebApi.Contexts;
 using Exo.WebApi.Repositories;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ExoContext, ExoContext>();
 builder.Services.AddControllers();
 builder.Services.AddTransient<ProjetoRepository, ProjetoRepository>();
+builder.Services.AddTransient<UsuarioRepository, UsuarioRepository>();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "NomeDaSuaAPI", Version = "v1" });
+    // Adicione os comentários XML do seu projeto, se houverem
+    // c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "NomeDoSeuProjeto.xml"));
+});
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "NomeDaSuaAPI v1");
+    c.RoutePrefix = "swagger";
+});
+
 
 app.UseRouting();
 

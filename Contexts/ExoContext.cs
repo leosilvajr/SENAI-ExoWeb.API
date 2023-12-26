229490daf906e6e942475a3a478f0fd0f028000a
Leonardo <leosilvatanabi@gmail.com>
2023-12-26 16:22:32 -0300

    Atividade 3 - API com Context e método GET que retorna Lista

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,30 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {
+            
+        }
+
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options) 
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = LEO; Initial Catalog = ExoApi; Integrated Security = True; TrustServerCertificate = true");
+            }
+        }
+
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
---
+++ Controllers/ProjetosController.cs
@@ -0,0 +1,25 @@
+﻿using Exo.WebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exo.WebApi.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProjetosController : Controller
+    {
+
+        private readonly ProjetoRepository  _projetoRepository;
+
+        public ProjetosController(ProjetoRepository projetoRepository)
+        {
+            _projetoRepository = projetoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        
+        {
+            return Ok(_projetoRepository.Listar());
+        }
+    }
+}
